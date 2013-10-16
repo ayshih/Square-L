9231dbc248a5ba1885359bc725d0a7f546de34df
Albert Y. Shih <ayshih@gmail.com>
2013-10-15 21:08:33 -0400

    Replacing tabs with spaces

--- Square-L/Crypto/Crypto.cpp
+++ Square-L/Crypto/Crypto.cpp
@@ -17,76 +17,76 @@ CryptoRuntimeComponent::CryptoRuntimeComponent()
 }
 
 void CryptoRuntimeComponent::CreateKeyPair(Array<unsigned char>^* publicKey,
-										   Array<unsigned char>^* privateKey,
-										   const Array<unsigned char>^ seed)
+                                           Array<unsigned char>^* privateKey,
+                                           const Array<unsigned char>^ seed)
 {
-	auto _publicKey = ref new Array<unsigned char>(32);
-	auto _privateKey = ref new Array<unsigned char>(64);
+    auto _publicKey = ref new Array<unsigned char>(32);
+    auto _privateKey = ref new Array<unsigned char>(64);
 
-	ed25519_create_keypair(_publicKey->Data, _privateKey->Data, seed->Data);
+    ed25519_create_keypair(_publicKey->Data, _privateKey->Data, seed->Data);
 
-	*publicKey = _publicKey;
-	*privateKey = _privateKey;
+    *publicKey = _publicKey;
+    *privateKey = _privateKey;
 }
 
 Array<unsigned char>^ CryptoRuntimeComponent::CreateSignature(const Array<unsigned char>^ message,
-															  const Array<unsigned char>^ publicKey,
-															  const Array<unsigned char>^ privateKey)
+                                                              const Array<unsigned char>^ publicKey,
+                                                              const Array<unsigned char>^ privateKey)
 {
-	auto _signature = ref new Array<unsigned char>(64);
+    auto _signature = ref new Array<unsigned char>(64);
 
-	ed25519_sign(_signature->Data, message->Data, message->Length, publicKey->Data, privateKey->Data);
+    ed25519_sign(_signature->Data, message->Data, message->Length, publicKey->Data, privateKey->Data);
 
-	return _signature;
+    return _signature;
 }
 
 bool CryptoRuntimeComponent::VerifySignature(const Platform::Array<unsigned char>^ signature,
-											 const Platform::Array<unsigned char>^ message,
-											 const Platform::Array<unsigned char>^ publicKey)
+                                             const Platform::Array<unsigned char>^ message,
+                                             const Platform::Array<unsigned char>^ publicKey)
 {
-	int result = ed25519_verify(signature->Data, message->Data, message->Length, publicKey->Data);
-	return (result == 1);
+    int result = ed25519_verify(signature->Data, message->Data, message->Length, publicKey->Data);
+    return (result == 1);
 }
 
 Array<unsigned char>^ CryptoRuntimeComponent::SCrypt(const Array<unsigned char>^ password,
-													 const Array<unsigned char>^ salt,
-													 int log2_N, int r, int p)
+                                                     const Array<unsigned char>^ salt,
+                                                     int log2_N, int r, int p)
 {
-	auto _output = ref new Array<unsigned char>(32);
+    auto _output = ref new Array<unsigned char>(32);
 
-	errno = 0;
-	if (crypto_scrypt(password->Data, password->Length, salt->Data, salt->Length, 1 << log2_N, r, p, _output->Data, 32) == -1)
-	{
-		switch (errno)
-		{
-		case EFBIG:
-			throw ref new Platform::InvalidArgumentException("r*p >= 2^30");
-		case EINVAL:
-			throw ref new Platform::InvalidArgumentException("N not a power of 2");
-		case ENOMEM:
-			throw ref new Platform::OutOfMemoryException("Implied memory request will be too large");
-		default:
-			throw ref new Platform::OutOfMemoryException("Failed to allocate memory");
-		}
-	}
+    errno = 0;
+    if (crypto_scrypt(password->Data, password->Length, salt->Data, salt->Length, 1 << log2_N, r, p, _output->Data, 32) == -1)
+    {
+        switch (errno)
+        {
+        case EFBIG:
+            throw ref new Platform::InvalidArgumentException("r*p >= 2^30");
+        case EINVAL:
+            throw ref new Platform::InvalidArgumentException("N not a power of 2");
+        case ENOMEM:
+            throw ref new Platform::OutOfMemoryException("Implied memory request will be too large");
+        default:
+            throw ref new Platform::OutOfMemoryException("Failed to allocate memory");
+        }
+    }
 
-	return _output;
+    return _output;
 }
 
 Windows::Foundation::IAsyncOperation<Object^>^ CryptoRuntimeComponent::SCryptAsync(const Array<unsigned char>^ password,
-																				   const Array<unsigned char>^ salt,
-																				   int log2_N, int r, int p)
+                                                                                   const Array<unsigned char>^ salt,
+                                                                                   int log2_N, int r, int p)
 {
-	return concurrency::create_async([this, password, salt, log2_N, r, p] () -> Object^ { return SCrypt(password, salt, log2_N, r, p); });
+    return concurrency::create_async([this, password, salt, log2_N, r, p] () -> Object^ { return SCrypt(password, salt, log2_N, r, p); });
 }
 
 Array<unsigned char>^ CryptoRuntimeComponent::PBKDF2_HMACSHA256(const Array<unsigned char>^ password,
-																const Array<unsigned char>^ salt,
-																int iterations)
+                                                                const Array<unsigned char>^ salt,
+                                                                int iterations)
 {
-	auto _output = ref new Array<unsigned char>(32);
+    auto _output = ref new Array<unsigned char>(32);
 
-	PBKDF2_SHA256(password->Data, password->Length, salt->Data, salt->Length, iterations, _output->Data, 32);
+    PBKDF2_SHA256(password->Data, password->Length, salt->Data, salt->Length, iterations, _output->Data, 32);
 
-	return _output;
+    return _output;
 }
--- Square-L/Crypto/Crypto.h
+++ Square-L/Crypto/Crypto.h
@@ -2,30 +2,30 @@
 
 namespace Crypto
 {
-	public ref class CryptoRuntimeComponent sealed
-	{
-	public:
-		CryptoRuntimeComponent();
+    public ref class CryptoRuntimeComponent sealed
+    {
+    public:
+        CryptoRuntimeComponent();
 
-		void CreateKeyPair(Platform::Array<unsigned char>^* publicKey,
-			Platform::Array<unsigned char>^* privateKey,
-			const Platform::Array<unsigned char>^ seed);
-		Platform::Array<unsigned char>^ CreateSignature(const Platform::Array<unsigned char>^ message,
-			const Platform::Array<unsigned char>^ publicKey,
-			const Platform::Array<unsigned char>^ privateKey);
-		bool VerifySignature(const Platform::Array<unsigned char>^ signature,
-			const Platform::Array<unsigned char>^ message,
-			const Platform::Array<unsigned char>^ public_key);
+        void CreateKeyPair(Platform::Array<unsigned char>^* publicKey,
+            Platform::Array<unsigned char>^* privateKey,
+            const Platform::Array<unsigned char>^ seed);
+        Platform::Array<unsigned char>^ CreateSignature(const Platform::Array<unsigned char>^ message,
+            const Platform::Array<unsigned char>^ publicKey,
+            const Platform::Array<unsigned char>^ privateKey);
+        bool VerifySignature(const Platform::Array<unsigned char>^ signature,
+            const Platform::Array<unsigned char>^ message,
+            const Platform::Array<unsigned char>^ public_key);
 
-		Platform::Array<unsigned char>^ SCrypt(const Platform::Array<unsigned char>^ password,
-			const Platform::Array<unsigned char>^ salt,
-			int log2_N, int r, int p);
-		Windows::Foundation::IAsyncOperation<Platform::Object^>^ SCryptAsync(const Platform::Array<unsigned char>^ password,
-			const Platform::Array<unsigned char>^ salt,
-			int log2_N, int r, int p);
+        Platform::Array<unsigned char>^ SCrypt(const Platform::Array<unsigned char>^ password,
+            const Platform::Array<unsigned char>^ salt,
+            int log2_N, int r, int p);
+        Windows::Foundation::IAsyncOperation<Platform::Object^>^ SCryptAsync(const Platform::Array<unsigned char>^ password,
+            const Platform::Array<unsigned char>^ salt,
+            int log2_N, int r, int p);
 
-		Platform::Array<unsigned char>^ PBKDF2_HMACSHA256(const Platform::Array<unsigned char>^ password,
-			const Platform::Array<unsigned char>^ salt,
-			int iterations);
-	};
+        Platform::Array<unsigned char>^ PBKDF2_HMACSHA256(const Platform::Array<unsigned char>^ password,
+            const Platform::Array<unsigned char>^ salt,
+            int iterations);
+    };
 }
